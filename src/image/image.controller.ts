236654db4771ba7d2d1889f060ca175f83696a1a
Gustavo <gumrqss@gmail.com>
2023-05-22 23:17:29 -0300

    feat: Response model-localpath

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -16,28 +16,14 @@ export class ImageController {
   @Post('save')
   async postImage(@Body() { image, compress }: { image: string, compress: number }) {
     try {
-        
-      /* if (typeof imageData.compress != 'number') throw new UnprocessableEntityException('Unprocessable Entity'); */
       const imagePath = await this.imageService.downloadAndSaveImage(image);
       const resizedImageBuffer = await this.imageService.imageResize(imagePath, compress, image);
       return resizedImageBuffer;
-      /* const result = await this.imageService.imageResize(image, compress);
-      return { success: true }; */
+     
     } catch (error) {
-      /* if(error instanceof UnprocessableEntityException){
-        throw new HttpException('Unprocessable Content', HttpStatus.UNPROCESSABLE_ENTITY);
-      } */
+     
       console.log(error)
       throw new HttpException('Error resizing image', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   } 
 }
-
-/* const imageName = 'thumb.jpg'; // Defina o nome desejado para o arquivo
-      const imagePath = path.join(__dirname, '..', 'images', imageName);
-
-      fs.writeFileSync(imagePath, result);
-
-      const savedimage = `/image/${imageName}`; // Caminho do link da imagem salva
-      console.log(savedimage, ' deu ?')
-      return { message: 'Image saved successfully', image: savedimage }; */
--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -6,45 +6,63 @@ import * as probe from 'probe-image-size';
 import * as fs from 'fs'
 import axios from 'axios';
 import * as path from 'path';
+import ExifReader from 'exif-reader';
+
 @Injectable()
 export class ImageService {
   constructor(private readonly imageRepository: ImageRepository) {}
-
+  
   async downloadAndSaveImage(image: string): Promise<string> {
     try {
-        const response = await axios.get(image, { responseType: 'arraybuffer' });
-        const imageName = path.basename(image, path.extname(image)); // Defina o nome desejado para o arquivo
-        const imagePath = path.join(__dirname, '..', '..', 'images', `${imageName}_original.jpg`);
-          if (!fs.existsSync(path.dirname(imagePath))) {
-            fs.mkdirSync(path.dirname(imagePath), { recursive: true });
-          }
-          fs.writeFileSync(imagePath, Buffer.from(response.data));
-          console.log('Imagem salva:', imagePath);
-        return imagePath;
+      const response = await axios.get(image, { responseType: 'arraybuffer' });
+      const imageName = path.basename(image, path.extname(image)); // Defina o nome desejado para o arquivo
+      const imagePath = path.join(__dirname, '..', '..', 'images', `${imageName}_original.jpg`);
+  
+      if (!fs.existsSync(path.dirname(imagePath))) {
+        fs.mkdirSync(path.dirname(imagePath), { recursive: true });
+      }
+  
+      fs.writeFileSync(imagePath, Buffer.from(response.data));
+      console.log('Imagem salva:', imagePath);
+      
+
+      return imagePath;
     } catch (error) {
-        console.log(error);
-        throw new Error('Error downloading and saving image');
+      console.log(error);
+      throw new Error('Error downloading and saving image');
     }
   }
+  
 
-  async imageResize(imagePath: string, compress: number, image: string): Promise<Buffer> {
+  async imageResize(imagePath: string, compress: number, image: string) {
     console.log(imagePath, 'oxiii');
     try {
       const imageInfo = await probe(image);
       const { width, height } = imageInfo;
       console.log(imageInfo, 'olha aiiii');
-
+  
       const fileName = path.basename(image, path.extname(image));
       const outputPath = path.join(__dirname, '..', '..', 'images', `${fileName}_thumb.jpg`);
-    
+  
       await sharp(imagePath)
         .resize(720, null)
         .toFile(outputPath);
-      
-      return ;
+    
+      const response = {
+        "localpath": {
+          "original": imagePath,
+          "thumb": outputPath,
+        },
+        "metadata": {
+          "ALL_EXIF_DATA_KEY":'haha'
+      }
+      };
+  
+      return response;
     } catch (error) {
       console.log(error);
       throw new Error('Error resizing image');
     }
   }
-}
+  }
+  
